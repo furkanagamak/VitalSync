1d7f2b2429e9506a7af78c54ad81a106a70c8956
zuxiangw <zuxiangwang2002@gmail.com>
2024-04-03 17:34:07 -0400

    notification box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import toast, { Toaster } from "react-hot-toast";
 import Navbar from "./components/navbar.jsx";
 import Resources from "./components/resources/Resources";
 import AdminActions from "./components/AdminActions";
-import NotificationDropDown from "./components/notifications/NotificationDropDown";
+import NotificationBox from "./components/notifications/NotificationBox";
 import CreateAccount from "./components/CreateAccount";
 
 const notify = () => toast("Here is your toast.");
@@ -12,7 +12,7 @@ function App() {
     <>
       <Toaster />
       <Navbar />
-      {/* <NotificationDropDown /> */}
+      <NotificationBox />
     </>
   );
 }
