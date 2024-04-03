a5c1e9e48661d965ddc1a42ea79c97bd273d8aaf
furkanagamak <furkanagamak06@gmail.com>
2024-04-03 00:41:45 -0400

    Implemented adding and modifying sections.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import ModifyProcedureTemplateForm from "./modifyProcedureTemplate.js";
 import ProcessTemplateManagement from "./processTemplates.js";
 import CreateProcessTemplateForm from "./createProcessTemplate.js";
 import ModifyProcessTemplateForm from "./modifyProcessTemplate.js";
+import AddSectionForm from "./addSection.js";
+import ModifySectionForm from "./modifySection.js";
 const notify = () => toast("Here is your toast.");
 
 function App() {
@@ -13,7 +15,7 @@ function App() {
     <>
       <Toaster />
       <Navbar />
-      <ModifyProcessTemplateForm />
+      <ModifySectionForm />
     </>
   );
 }
