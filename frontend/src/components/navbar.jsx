bb2555c03e4b6e0b879bb6c85a5dd1e823abcec1
zuxiangw <zuxiangwang2002@gmail.com>
2024-04-04 23:31:46 -0400

    navbar linking updated

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -2,7 +2,7 @@ import { CiBellOn } from "react-icons/ci";
 import { useState } from "react";
 import { IoMenu } from "react-icons/io5";
 import NotificationDropDown from "./notifications/NotificationDropDown";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -29,15 +29,20 @@ const Header = () => {
 };
 
 const Tabs = () => {
-  const navigate = useNavigate(); 
-  const navigateToRoster = () => navigate('/Roster');
+  const navigate = useNavigate();
+  const navigateToRoster = () => navigate("/Roster");
+  const navigateToAdminActions = () => navigate("/adminActions");
   return (
-    <div className="border-x-2 border-white text-2xl flex text-center">
-      <section className="border-r-2 px-8 flex pb-2" onClick={navigateToRoster}>
-        <p className="my-auto">Roster</p>
+    <div className="border-x-2 border-white text-2xl hidden md:flex text-center">
+      <section className="border-r-2 px-8 flex pb-2">
+        <button className="my-auto" onClick={navigateToRoster}>
+          Roster
+        </button>
       </section>
       <section className="px-8 flex pb-2">
-        <p className="my-auto">Admin Actions</p>
+        <button onClick={navigateToAdminActions} className="my-auto">
+          Admin Actions
+        </button>
       </section>
     </div>
   );
@@ -47,12 +52,15 @@ const UserNav = () => {
   const navigate = useNavigate();
 
   const handleNavigate = () => {
-    navigate('/Profile');
+    navigate("/Profile");
   };
 
   return (
-    <div className="ml-auto flex space-x-4 items-center mx-2"  onClick={handleNavigate}>
-      <p className="text-2xl">John Smith</p>
+    <div
+      className="ml-auto space-x-4 flex items-center mx-2"
+      onClick={handleNavigate}
+    >
+      <p className="text-2xl hidden md:block">John Smith</p>
       <img src="/logo512.png" className="h-12 w-12" />
     </div>
   );
@@ -85,15 +93,23 @@ const Menu = () => {
     setIsOpen((isOpen) => !isOpen);
   };
 
+  const navigate = useNavigate();
+  const navigateToRoster = () => navigate("/Roster");
+  const navigateToAdminActions = () => navigate("/adminActions");
+
   return (
     <>
       <button className="md:hidden mr-2" onClick={triggerIsOpen}>
         <IoMenu className="w-12 h-12 text-white" />
       </button>
       {isOpen && (
-        <div className="absolute right-0 top-20 bg-primary text-2xl text-center z-10">
-          <div className="border-b-2 p-2">Roster</div>
-          <div className="border-b-2 p-2">Admin Actions</div>
+        <div className="absolute right-0 top-20 bg-primary text-2xl text-center z-10 flex flex-col">
+          <button onClick={navigateToRoster} className="border-b-2 p-2">
+            Roster
+          </button>
+          <button onClick={navigateToAdminActions} className="border-b-2 p-2">
+            Admin Actions
+          </button>
         </div>
       )}
     </>
