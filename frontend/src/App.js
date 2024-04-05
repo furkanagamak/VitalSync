818aac62ed335057d4a0ba3ed051bdfa6490cf6b
furkanagamak <furkanagamak06@gmail.com>
2024-04-04 22:05:49 -0400

    Change app.css and app.js for merging.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,5 @@
 import toast, { Toaster } from "react-hot-toast";
 import Navbar from "./components/navbar.jsx";
-import ProcedureTemplateManagement from "./components/procedureTemplates.js";
-import CreateProcedureTemplateForm from "./components/createProcedureTemplate.js";
-import ModifyProcedureTemplateForm from "./components/modifyProcedureTemplate.js";
-import ProcessTemplateManagement from "./components/processTemplates.js";
-import CreateProcessTemplateForm from "./components/createProcessTemplate.js";
-import ModifyProcessTemplateForm from "./components/modifyProcessTemplate.js";
-import AddSectionForm from "./components/createSection.js";
-import ModifySectionForm from "./components/modifySection.js";
 const notify = () => toast("Here is your toast.");
 
 function App() {
@@ -15,7 +7,6 @@ function App() {
     <>
       <Toaster />
       <Navbar />
-      <ProcessTemplateManagement />
     </>
   );
 }
