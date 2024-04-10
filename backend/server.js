25ed8819041905eba95bbbbc43dab7484746a951
furkanagamak <furkanagamak06@gmail.com>
2024-04-09 21:04:48 -0400

    Setup basecode for the backend.

---
+++ backend/server.js
@@ -0,0 +1,54 @@
+const cookie = require("cookie");
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const { Socket } = require("dgram");
+
+const fs = require("fs").promises;
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected."))
+  .catch((err) => {
+    console.error("Database connection error:", err);
+    process.exit(1);
+  });
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const server = http.createServer(app);
+/* const io = socketIo(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL,
+  },
+}); */
+
+const PORT = 5000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
