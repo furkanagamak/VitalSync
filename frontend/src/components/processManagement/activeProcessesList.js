e2e7e2866b52de8497f0205f86782590974a41ec
zuxiangw <zuxiangwang2002@gmail.com>
2024-05-03 14:10:14 -0400

    active process tests

--- frontend/src/components/processManagement/activeProcessesList.js
+++ frontend/src/components/processManagement/activeProcessesList.js
@@ -1,20 +1,19 @@
-
-import React, { useState, useEffect } from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React, { useState, useEffect } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { FaSearch } from "react-icons/fa";
-import { useTheme } from '@mui/material/styles';
-import { Outlet, useLocation, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
+import { useTheme } from "@mui/material/styles";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#8e0000',
+      main: "#8e0000",
     },
     secondary: {
-      main: '#f5f5dc',
+      main: "#f5f5dc",
     },
   },
 });
@@ -44,29 +43,52 @@ function ProcessCell({ process, onModifyClick, onViewClick, pid }) {
     navigate(`/processManagement/modifyProcess/landing/${process.processID}`);
   };
 
-  const navigate = useNavigate(); 
-
+  const navigate = useNavigate();
 
   return (
     <div className="flex overflow-hidden bg-primary rounded-2xl p-7 mb-5 text-white text-2xl m-5">
       <div className="flex-[1.5] border-r border-white min-w-0 pr-4">
         <div className="space-y-1">
-          <p className="truncate"><span className="underline">Patient:</span> {process.patientFullName}</p>
-          <p className="truncate"><span className="underline">Process:</span> {process.processName}</p>
-          <p className="truncate"><span className="underline">Process ID:</span> {process.processID}</p>
+          <p className="truncate">
+            <span className="underline">Patient:</span>{" "}
+            {process.patientFullName}
+          </p>
+          <p className="truncate">
+            <span className="underline">Process:</span> {process.processName}
+          </p>
+          <p className="truncate">
+            <span className="underline">Process ID:</span> {process.processID}
+          </p>
         </div>
       </div>
       <div className="flex-[2] border-r border-white min-w-0 px-4">
         <div className="space-y-1">
-          <p className="truncate"><span className="underline">Current Procedure:</span> {process.currentProcedure}</p>
-          <p className="truncate"><span className="underline">Next Procedure:</span> {process.nextProcedure}</p>
+          <p className="truncate">
+            <span className="underline">Current Procedure:</span>{" "}
+            {process.currentProcedure}
+          </p>
+          <p className="truncate">
+            <span className="underline">Next Procedure:</span>{" "}
+            {process.nextProcedure}
+          </p>
         </div>
       </div>
       <div className="flex-[1] pl-4 flex justify-end items-start min-w-0">
         <div className="flex flex-col space-y-2">
-          <button className="bg-green-500 hover:bg-green-600 rounded-full px-10 py-1 text-center" onClick={handleViewClickID}>View</button>
-          <button className="bg-red-500 hover:bg-red-600 rounded-full px-4 py-1 text-center"
- onClick={handleModifyClickID}>Modify</button>
+          <button
+            className="bg-green-500 hover:bg-green-600 rounded-full px-10 py-1 text-center"
+            onClick={handleViewClickID}
+            id={`activeProcessViewBtn-${process.processID}`}
+          >
+            View
+          </button>
+          <button
+            className="bg-red-500 hover:bg-red-600 rounded-full px-4 py-1 text-center"
+            onClick={handleModifyClickID}
+            id={`activeProcessModifyBtn-${process.processID}`}
+          >
+            Modify
+          </button>
         </div>
       </div>
     </div>
@@ -76,21 +98,20 @@ function ProcessCell({ process, onModifyClick, onViewClick, pid }) {
 export function ActiveProcessesList() {
   const [processes, setProcesses] = useState([]);
   const [page, setPage] = useState(1);
-  const [searchInput, setSearchInput] = useState('');
+  const [searchInput, setSearchInput] = useState("");
   const [filteredProcesses, setFilteredProcesses] = useState([]);
 
-
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchProcesses = async () => {
       try {
-        const response = await axios.get('/processInstancesActive');
+        const response = await axios.get("/processInstancesActive");
         setProcesses(response.data);
         setFilteredProcesses(response.data);
         console.log(response.data);
       } catch (error) {
-        console.error('Failed to fetch process instances', error);
+        console.error("Failed to fetch process instances", error);
       }
     };
 
@@ -99,13 +120,17 @@ export function ActiveProcessesList() {
 
   useEffect(() => {
     const filtered = processes.filter(
-      p => p.processID.includes(searchInput.trim()) || p.patientFullName.toLowerCase().includes(searchInput.trim().toLowerCase())
+      (p) =>
+        p.processID.includes(searchInput.trim()) ||
+        p.patientFullName
+          .toLowerCase()
+          .includes(searchInput.trim().toLowerCase())
     );
     setFilteredProcesses(filtered);
   }, [searchInput, processes]);
 
   const handleChange = (event, value) => {
-      setPage(value);
+    setPage(value);
   };
 
   const handleModifyClick = (pid) => {
@@ -118,46 +143,56 @@ export function ActiveProcessesList() {
 
   const indexOfLastItem = page * 8;
   const indexOfFirstItem = indexOfLastItem - 8;
-  const currentItems = filteredProcesses.slice(indexOfFirstItem, indexOfLastItem);
+  const currentItems = filteredProcesses.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
 
   return (
     <ThemeProvider theme={theme}>
       <SearchBar onChange={setSearchInput} />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 px-10 py-5">
-  {currentItems.length > 0 ? (
-    currentItems.map((process, index) => (
-      <ProcessCell key={index} process={process} onModifyClick={handleModifyClick} onViewClick={handleViewClick} pid={process.processID}/>
-    ))
-  ) : (
-    <div className="col-span-full text-center text-xl text-primary">
-      No results found
-    </div>
-  )}
-</div>
+        {currentItems.length > 0 ? (
+          currentItems.map((process, index) => (
+            <ProcessCell
+              key={index}
+              process={process}
+              onModifyClick={handleModifyClick}
+              onViewClick={handleViewClick}
+              pid={process.processID}
+            />
+          ))
+        ) : (
+          <div className="col-span-full text-center text-xl text-primary">
+            No results found
+          </div>
+        )}
+      </div>
       <Stack spacing={2} alignItems="center" className="py-5">
-        <Pagination 
+        <Pagination
           color="primary"
           size="large"
-          count={Math.ceil(filteredProcesses.length / 8)} 
-          page={page} 
-          onChange={handleChange} 
-          showFirstButton 
-          showLastButton 
+          count={Math.ceil(filteredProcesses.length / 8)}
+          page={page}
+          onChange={handleChange}
+          showFirstButton
+          showLastButton
           sx={{
             ".MuiPaginationItem-root": {
-              color: '#8e0000', 
+              color: "#8e0000",
             },
             ".Mui-selected": {
-              backgroundColor: '#8e0000',
-              color: 'white',
-              '&:hover': {
-                backgroundColor: '#8e0000', 
+              backgroundColor: "#8e0000",
+              color: "white",
+              "&:hover": {
+                backgroundColor: "#8e0000",
               },
             },
             ".MuiPaginationItem-ellipsis": {
-              color: '#8e0000',
-            }
-          }}/>
+              color: "#8e0000",
+            },
+          }}
+        />
       </Stack>
     </ThemeProvider>
   );
