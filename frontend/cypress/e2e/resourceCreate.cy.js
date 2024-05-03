c3c69247ccf9a7fa69a67ec3f5ffa5f91e3d840e
zuxiangw <zuxiangwang2002@gmail.com>
2024-05-03 04:25:24 -0400

    resource create test additions

--- frontend/cypress/e2e/resourceCreate.cy.js
+++ frontend/cypress/e2e/resourceCreate.cy.js
@@ -5,17 +5,16 @@ describe("Resource Create", () => {
     cy.get("#passwordInput").type("password123");
     cy.get("button").contains("Sign in").click();
     cy.wait(1000);
-    cy.visit("/resources");
-  });
-  it("loading and navigating", () => {
     cy.visit("http://localhost:3000/resources/create");
+  });
+
+  it("loading and navigating", () => {
     cy.contains("Equipment");
     cy.contains("Role");
     cy.contains("Spaces");
   });
-  it("form flow", () => {
-    cy.visit("http://localhost:3000/resources/create");
 
+  it("form flow", () => {
     // select tests
     cy.get("#selectEquipmentsBtn").click();
     cy.contains("Equipment");
@@ -47,5 +46,41 @@ describe("Resource Create", () => {
     cy.get("#descriptionInp").should("have.value", "This is a patient bed");
 
     cy.get("#submitBtn").click();
+
+    cy.contains("The resource has been successfully created!");
+  });
+
+  it("create test equipment", () => {
+    cy.get("#selectEquipmentsBtn").click();
+    cy.contains("Equipment");
+
+    // form insertions
+    cy.get("#nameInp").type("Syringe");
+    cy.get("#nameInp").should("have.value", "Syringe");
+
+    cy.get("#locationInp").type("Room 200A");
+    cy.get("#locationInp").should("have.value", "Room 200A");
+
+    cy.get("#descriptionInp").type("This is a syringe");
+    cy.get("#descriptionInp").should("have.value", "This is a syringe");
+
+    cy.get("#submitBtn").click();
+
+    cy.contains("The resource has been successfully created!");
+  });
+
+  it("create role", () => {
+    cy.get("#selectRoleBtn").click();
+    cy.contains("Roles");
+
+    cy.get("#nameInp").type("Administrators");
+    cy.get("#nameInp").should("have.value", "Administrators");
+
+    cy.get("#descriptionInp").type("This is a admin");
+    cy.get("#descriptionInp").should("have.value", "This is a admin");
+
+    cy.get("#submitBtn").click();
+
+    cy.contains("The newly requested role is created!");
   });
 });
